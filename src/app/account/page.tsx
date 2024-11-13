cb5cd012cfbeaf2d352cca4adf0a4cf2fe5bc180
Janus <ktr84759@gmail.com>
2024-11-13 14:41:44 +0800

    commit all modify.

---
+++ src/app/account/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+export default function AccountPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1>Account Page</h1>
+      {/* 您的帳戶頁面內容 */}
+    </div>
+  );
+}
